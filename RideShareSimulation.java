dc673ee39e4360cf1fc70634751eb440733f21fa
Evelyn Kim <ekim25@students.polytechnic.org>
2025-03-12 20:56:48 -0700

    run scenario method in progress.

--- RideShareSimulation.java
+++ RideShareSimulation.java
@@ -8,3 +8,20 @@ public class RideShareSimulation {
         System.out.println("\n=== CONCLUSION ===");
         System.out.println("More cars = better service.");
     }
+
+    private static void runScenario(int numCars, int numPassengers, int numStations) {
+        System.out.println("\n=== SCENARIO: " + numCars + " cars, " + numPassengers + " passengers ===");
+        
+        Road road = new Road();
+        
+        System.out.println("Creating passengers...");
+        road.populateStations(numPassengers);
+        
+        System.out.println("Creating cars...");
+        road.populateCars(numCars);
+        
+        System.out.println("Running simulation...");
+        int maxSteps = 100;
+        
+        int previousCompleted = 0;
+        int stepsWithoutChange = 0;
